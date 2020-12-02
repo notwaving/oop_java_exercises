53d6a89baaff4fe03bf7408a14cd950be9a1f323
notwaving <samanthaburke.uk@gmail.com>
2020-12-02 17:58:16 +0000

    initial commit

---
+++ src/DomesticCat.java
@@ -0,0 +1,16 @@
+package com.techreturners.cats;
+
+class DomesticCat extends Animal implements Cat {
+        
+    public DomesticCat(){
+
+            this.isAsleep = false;
+            this.averageHeight = 23;
+            this.setting = "domestic";
+            this.burp = "Purrrrrrr";
+
+    }
+}
+
+
+
--- src/main/java/com/techreturners/cats/Cat.java
+++ src/main/java/com/techreturners/cats/Cat.java
@@ -2,4 +2,16 @@ package com.techreturners.cats;
 
 interface Cat {
 
+  boolean isAsleep();
+
+  void goToSleep();
+
+  void wakeUp();
+
+  int getAverageHeight();
+
+  String getSetting();
+
+  String eat();
+
 }
